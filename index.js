df7a9f41b1a252ccf6d1cfa7020e833e86a5d812
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-11-12 23:58:09 +0300

    replace nodemon with browser-sync

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const reload = require('reload');
+// const reload = require('reload');
 const PORT = 3000;
 
 const app = express();
-app.use('/', express.static(__dirname + '/dist/'));
+app.use('/', express.static(__dirname + 'dist'));
 
-reload(app).then(() => {
-  app.listen(PORT, function () {
-    console.log(`server started at http://localhost:${PORT}`)
-  })
-}).catch(function (err) {
-  console.error('Reload could not start', err)
+app.listen(PORT, function () {
+  console.log(`server started at http://localhost:${PORT}`)
 })
