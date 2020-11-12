40d3383ead291025b34fafed5902a138c41ce4b6
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-11-13 00:00:25 +0300

    rename dist to static

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 3000;
 
 const app = express();
-app.use('/', express.static(__dirname + 'dist'));
+app.use('/', express.static(__dirname + 'static'));
 
 app.listen(PORT, function () {
   console.log(`server started at http://localhost:${PORT}`)
