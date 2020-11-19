92f1e6ba3dd0616692900ff93016dbf2fb261338
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-11-19 15:42:22 +0300

    refactoring

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line no-unused-vars
-const openDialog = (src, elementId = 'dialog', parseTag = 'main') => {
+/* eslint-disable no-unused-vars, no-console */
+
+const openDialog = (src, elementId = 'mpy_dialog', parseTag = 'main') => {
   const dialog = document.getElementById(elementId);
   if (!dialog) {
     // static
@@ -11,8 +12,6 @@ const openDialog = (src, elementId = 'dialog', parseTag = 'main') => {
     dialog.style.position = 'fixed';
     dialog.style.top = '0';
     dialog.style.left = '0';
-    dialog.style.opacity = '0';
-    dialog.hidden = false;
     fetch(src)
       .then((response) => response.text())
       .then((page) => {
@@ -21,26 +20,23 @@ const openDialog = (src, elementId = 'dialog', parseTag = 'main') => {
         if (content) {
           const [, html] = content;
           dialog.innerHTML = html;
-          setTimeout(() => {
-            dialog.style.opacity = '1';
-          }, 0);
+          dialog.hidden = false;
+          dialog.style.opacity = '1';
         }
         else {
           dialog.hidden = true;
-          // eslint-disable-next-line no-console
           console.warn(`cant parse tag '${parseTag}' from url '${src}'`);
         }
       })
       .catch((e) => {
         dialog.hidden = true;
-        // eslint-disable-next-line no-console
         console.error(e);
       });
   }
   return false;
 };
 
-function closeDialog(elementId = 'dialog') {
+function closeDialog(elementId = 'mpy_dialog') {
   const dialog = document.getElementById(elementId);
   if (!dialog) {
     // static
@@ -50,19 +46,17 @@ function closeDialog(elementId = 'dialog') {
     dialog.style.opacity = '0';
     setTimeout(() => {
       dialog.hidden = true;
-    }, 500, dialog);
+    }, 300);
   }
   return false;
 }
 
-// eslint-disable-next-line no-unused-vars
 function submitForm(form) {
   const formData = new FormData(form);
   const res = Array.from(formData.entries()).reduce((memo, pair) => ({
     ...memo,
     [pair[0]]: pair[1]
   }), {});
-  // eslint-disable-next-line no-console
   console.dir(res);
   closeDialog();
 }
