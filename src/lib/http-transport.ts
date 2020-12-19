e031882d7a42f649387f272617d4afb043d1f703
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-12-19 16:19:38 +0300

    add http-transport

---
+++ src/lib/http-transport.ts
@@ -0,0 +1,77 @@
+import { queryStringify, PlainObject } from './utils.js';
+
+type DataType = Record<string, string | number | Array<string> | Array<number> | boolean> | FormData;
+
+export type HttpRequestOptions = {
+  method?: string;
+  data?: DataType;
+  headers?: Record<string, string>;
+  timeout?: number;
+}
+
+export default class HttpTransport {
+  METHODS = Object.freeze({
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    PATCH: 'PATCH',
+    DELETE: 'DELETE',
+  });
+
+  get = (url: string, options: HttpRequestOptions = {}): Promise<XMLHttpRequest> => this.request(url, {
+    ...options,
+    method: this.METHODS.GET,
+  }, options.timeout)
+
+  put = (url: string, options: HttpRequestOptions = {}): Promise<XMLHttpRequest> => this.request(url, {
+    ...options,
+    method: this.METHODS.PUT,
+  }, options.timeout);
+
+  post = (url: string, options: HttpRequestOptions = {}): Promise<XMLHttpRequest> => this.request(url, {
+    ...options,
+    method: this.METHODS.POST,
+  }, options.timeout);
+
+  delete = (url: string, options: HttpRequestOptions = {}): Promise<XMLHttpRequest> => this.request(url, {
+    ...options,
+    method: this.METHODS.DELETE,
+  }, options.timeout);
+
+  request(url: string, options: HttpRequestOptions = {
+    headers: {},
+    data: {},
+    method: this.METHODS.GET,
+  }, timeout = 5000): Promise<XMLHttpRequest> {
+    const { headers, data, method } = options;
+
+    const sendURL = (method === this.METHODS.GET) ? `${url}?${queryStringify(data as PlainObject)}` : url;
+
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.timeout = timeout;
+      xhr.open(method as string, sendURL);
+      if (!headers || Object.keys(headers).length === 0) {
+        xhr.setRequestHeader('Content-Type', 'text/plain');
+      } else {
+        Object.keys(headers).forEach((header) => {
+          xhr.setRequestHeader(header, headers[header]);
+        });
+      }
+
+      xhr.onload = () => {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      if (method === this.METHODS.GET || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
+    });
+  }
+}
---
+++ static/lib/http-transport.js
@@ -0,0 +1,50 @@
+import { queryStringify } from './utils.js';
+export default class HttpTransport {
+    constructor() {
+        this.METHODS = Object.freeze({
+            GET: 'GET',
+            POST: 'POST',
+            PUT: 'PUT',
+            PATCH: 'PATCH',
+            DELETE: 'DELETE',
+        });
+        this.get = (url, options = {}) => this.request(url, Object.assign(Object.assign({}, options), { method: this.METHODS.GET }), options.timeout);
+        this.put = (url, options = {}) => this.request(url, Object.assign(Object.assign({}, options), { method: this.METHODS.PUT }), options.timeout);
+        this.post = (url, options = {}) => this.request(url, Object.assign(Object.assign({}, options), { method: this.METHODS.POST }), options.timeout);
+        this.delete = (url, options = {}) => this.request(url, Object.assign(Object.assign({}, options), { method: this.METHODS.DELETE }), options.timeout);
+    }
+    request(url, options = {
+        headers: {},
+        data: {},
+        method: this.METHODS.GET,
+    }, timeout = 5000) {
+        const { headers, data, method } = options;
+        const sendURL = (method === this.METHODS.GET) ? `${url}?${queryStringify(data)}` : url;
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.timeout = timeout;
+            xhr.open(method, sendURL);
+            if (!headers || Object.keys(headers).length === 0) {
+                xhr.setRequestHeader('Content-Type', 'text/plain');
+            }
+            else {
+                Object.keys(headers).forEach((header) => {
+                    xhr.setRequestHeader(header, headers[header]);
+                });
+            }
+            xhr.onload = () => {
+                resolve(xhr);
+            };
+            xhr.onabort = reject;
+            xhr.onerror = reject;
+            xhr.ontimeout = reject;
+            if (method === this.METHODS.GET || !data) {
+                xhr.send();
+            }
+            else {
+                xhr.send(JSON.stringify(data));
+            }
+        });
+    }
+}
+//# sourceMappingURL=http-transport.js.map
