3b3d7def5176675b03532c110d0b323246609d50
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-01-11 17:06:12 +0300

    added production webpack config

---
+++ webpack.production.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader', 'eslint-loader'],
+        exclude: [/node_modules/, /static/, /tests/],
+      },
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.scss'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './static/favicon.ico' },
+        { from: './static/index.html' },
+      ],
+    }),
+  ],
+};
