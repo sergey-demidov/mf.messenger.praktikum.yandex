e2a7e5d7b8a842f697b584e43c2a20de86781ddf
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-01-11 17:25:30 +0300

    fixed express exit code

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires, no-console */
 const express = require('express');
 const path = require('path');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000);
+
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  server.close(() => {
+    process.exit(0);
+  });
+}
+
+process.on('SIGTERM', () => shutDown());
+process.on('SIGINT', () => shutDown());
