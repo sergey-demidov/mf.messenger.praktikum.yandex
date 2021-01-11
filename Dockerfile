dffde179866a0a708ee3104acaf9568960b99f52
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-01-11 16:48:19 +0300

    added env.PORT variable to express

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM node:12-slim
 COPY ./ /app
 WORKDIR /app
 RUN npm ci && npm run build
-EXPOSE 3000
 CMD npm run serve
--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('fs');
-// const https = require('https');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static/index.html'));
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
-app.listen(3000);
-//
-// const coverage = express();
-// coverage.use('/', express.static(path.join(__dirname, 'coverage/lcov-report')));
-// coverage.listen(5000);
 
-// const key = fs.readFileSync('./example.com+5-key.pem');
-// const cert = fs.readFileSync('./example.com+5.pem');
-// const httpsServer = https.createServer({ key, cert }, app);
-// httpsServer.listen(3443, () => { console.log('listening on 3443'); });
+app.listen(process.env.PORT || 3000);
